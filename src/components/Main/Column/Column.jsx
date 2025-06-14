0c9c2c04478997d55646af406827c3c00ce962e1
OlmerOnline <olmeronline@mail.ru>
2025-06-14 14:48:30 +0400

    Добавил список карточек, отрисовал карточки и колонки в соответствии с макетом

--- src/components/Main/Column/Column.jsx
+++ src/components/Main/Column/Column.jsx
@@ -1,14 +1,18 @@
+import { cards } from "../../../../data";
 import CardsItem from "../CardsItem/CardsItem";
 
-function Column() {
+function Column({ status }) {
+  const listCard = cards.filter((card) => card.status === status);
+  const components = listCard.map((card) => (
+    <CardsItem key={card.id} card={card} />
+  ));
+
   return (
     <div className="main__column">
       <div className="column__title">
-        <p>Нужно сделать</p>
+        <p>{status}</p>
       </div>
-      <div className="cards">
-        <CardsItem />
-      </div>
+      <div className="cards">{components}</div>
     </div>
   );
 }
